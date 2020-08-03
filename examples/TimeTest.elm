96fa99892f34b7188b77514a16111c842ed9cb86
Kilo Meow <dest@disr.it>
2020-08-03 15:45:10 +0300

    elm-format applied

--- examples/TimeTest.elm
+++ examples/TimeTest.elm
@@ -1,17 +1,14 @@
 module Example.TimeTest exposing (..)
 
-
 import Browser
+import Date
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Moon
+import Round
 import Task
 import Time exposing (Month(..))
 import TimeUtils
-import Round
-
-import Moon
-
-import Date
 
 
 
@@ -19,12 +16,12 @@ import Date
 
 
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -32,16 +29,16 @@ main =
 
 
 type alias Model =
-  { zone : Time.Zone
-  , time : Time.Posix
-  }
+    { zone : Time.Zone
+    , time : Time.Posix
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model Time.utc (Time.millisToPosix 0)
-  , Task.perform AdjustTimeZone Time.here
-  )
+    ( Model Time.utc (Time.millisToPosix 0)
+    , Task.perform AdjustTimeZone Time.here
+    )
 
 
 
@@ -49,23 +46,22 @@ init _ =
 
 
 type Msg
-  = Tick Time.Posix
-  | AdjustTimeZone Time.Zone
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
 
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      ( { model | time = newTime }
-      , Cmd.none
-      )
+    case msg of
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
 
-    AdjustTimeZone newZone ->
-      ( { model | zone = newZone }
-      , Cmd.none
-      )
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
 
 
 
@@ -74,49 +70,81 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every 1000 Tick
+    Time.every 1000 Tick
+
+
 
 -- Lunar View
 
+
 viewMoonDay : Time.Zone -> Moon.MoonDay -> String
 viewMoonDay zone moon =
-    "{ day : " ++ String.fromInt moon.day ++ ", start : " ++ (viewTime zone moon.startTime) ++
-    ", end : " ++ (viewTime zone moon.endTime) ++ ", phase : " ++ (Round.round 2 moon.phase) ++ " }"
+    "{ day : "
+        ++ String.fromInt moon.day
+        ++ ", start : "
+        ++ viewTime zone moon.startTime
+        ++ ", end : "
+        ++ viewTime zone moon.endTime
+        ++ ", phase : "
+        ++ Round.round 2 moon.phase
+        ++ " }"
+
 
 viewDayInfo : Time.Zone -> Moon.DayInfo -> Html Msg
 viewDayInfo zone dayinfo =
-    div [ class "dayinfo"]
-        [ Date.toIsoString dayinfo.date |> ptext 
+    div [ class "dayinfo" ]
+        [ Date.toIsoString dayinfo.date |> ptext
         , div [ class "mooninfo" ] <| List.map (viewMoonDay zone >> ptext) dayinfo.moonInfo
         ]
+
 
 
 -- VIEW
 
 
 ptext : String -> Html Msg
-ptext t = p [] [text t] 
+ptext t =
+    p [] [ text t ]
 
 
 viewTime zone time =
-  let
-    hour   = String.fromInt (Time.toHour   zone time)
-    minute = String.fromInt (Time.toMinute zone time)
-    second = String.fromInt (Time.toSecond zone time)
-    day    = String.fromInt (Time.toDay    zone time)
-    month  = Time.toMonth zone time |> Date.monthToNumber |> String.fromInt
-    year   = String.fromInt (Time.toYear   zone time)         
-  in
-     day ++ "." ++ month ++ "." ++ year ++ " " ++ hour ++ ":" ++ minute ++ ":" ++ second 
+    let
+        hour =
+            String.fromInt (Time.toHour zone time)
+
+        minute =
+            String.fromInt (Time.toMinute zone time)
+
+        second =
+            String.fromInt (Time.toSecond zone time)
+
+        day =
+            String.fromInt (Time.toDay zone time)
+
+        month =
+            Time.toMonth zone time |> Date.monthToNumber |> String.fromInt
+
+        year =
+            String.fromInt (Time.toYear zone time)
+    in
+    day ++ "." ++ month ++ "." ++ year ++ " " ++ hour ++ ":" ++ minute ++ ":" ++ second
 
 
 view : Model -> Html Msg
 view model =
-    let 
-        start = TimeUtils.halfMonthBack    model.zone model.time
-        end   = TimeUtils.halfMonthForward model.zone model.time
+    let
+        start =
+            TimeUtils.halfMonthBack model.zone model.time
+
+        end =
+            TimeUtils.halfMonthForward model.zone model.time
     in
-        div [] <| List.map (viewDayInfo model.zone)
-                           (Moon.lunarCalendar model.zone { latitude  = 56.142406
-                                                          , longitude = 37.440216 }
-                                               start end)
+    div [] <|
+        List.map (viewDayInfo model.zone)
+            (Moon.lunarCalendar model.zone
+                { latitude = 56.142406
+                , longitude = 37.440216
+                }
+                start
+                end
+            )
--- src/TimeUtils.elm
+++ src/TimeUtils.elm
@@ -1,32 +1,65 @@
 module TimeUtils exposing (..)
 
 import Time exposing (Posix, posixToMillis)
-import Time.Extra exposing (Interval(..), add, diff, floor, ceiling)
+import Time.Extra exposing (Interval(..), add, ceiling, diff, floor)
 
-startOfDay = Time.Extra.floor Day
-endOfDay   = Time.Extra.ceiling Day
-addHours   = add Hour
-addMinutes = add Minute
 
-previousDay = add Day -1
-nextDay     = add Day 1
+startOfDay =
+    Time.Extra.floor Day
 
-halfMonthBack    = add Day -15
-halfMonthForward = add Day  15
+
+endOfDay =
+    Time.Extra.ceiling Day
+
+
+addHours =
+    add Hour
+
+
+addMinutes =
+    add Minute
+
+
+previousDay =
+    add Day -1
+
+
+nextDay =
+    add Day 1
+
+
+halfMonthBack =
+    add Day -15
+
+
+halfMonthForward =
+    add Day 15
+
 
 intervals : Interval -> Time.Zone -> Posix -> Posix -> List Posix
 intervals interval zone start end =
     let
-        startDay = startOfDay zone start
-        total    = diff interval zone startDay end
-    in
-        List.map (\i -> add interval i zone startDay) <| List.range 0 total
+        startDay =
+            startOfDay zone start
 
-hourIntervals = intervals Hour
-dayIntervals  = intervals Day
+        total =
+            diff interval zone startDay end
+    in
+    List.map (\i -> add interval i zone startDay) <| List.range 0 total
+
+
+hourIntervals =
+    intervals Hour
+
+
+dayIntervals =
+    intervals Day
+
 
 before : Posix -> Posix -> Bool
-before a b = (posixToMillis a) < (posixToMillis b)
+before a b =
+    posixToMillis a < posixToMillis b
+
 
 addFloatHours : Float -> Posix -> Posix
 addFloatHours dt posix =
